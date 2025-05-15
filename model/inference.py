b7d2219f2632a19da8d5aea305204b8c9ce793b9
Mohanna Torkashvand <74386667+mohannatd@users.noreply.github.com>
2025-05-15 02:04:07 +0100

    Add files via upload

---
+++ model/inference.py
@@ -0,0 +1,16 @@
+import torch
+import os
+from data.paths import INPUT_DIR, MODEL_PATH
+def check_cuda():
+    if not torch.cuda.is_available():
+        raise EnvironmentError("CUDA not available.")
+    print("CUDA is available.")
+
+def run_inference(output_path):
+    check_cuda()
+    print("Running inference... (placeholder)")
+    nnUNet_raw = os.path.join(INPUT_DIR, 'nnUNet_raw')
+    os.environ["nnUNet_results"] = output_path
+    os.environ["nnUNet_preprocessed"] = os.path.join('.')
+    os.environ["nnUNet_raw"] = os.path.join('.')
+    os.system(f"nnUNetv2_predict -i {nnUNet_raw} -o {output_path} -d 13 -c 3d_fullres -f all -tr nnUNetTrainerDiceCELoss_noSmooth")
