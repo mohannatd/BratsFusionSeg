2b98e25e0885ae29ccc31728100ad048168bb77d
mohan <mohantrkd@gmail.com>
2025-05-16 13:35:53 +0000

    Init commit

---
+++ model/inference.py
@@ -0,0 +1,15 @@
+import torch
+import os
+def check_cuda():
+    if not torch.cuda.is_available():
+        raise EnvironmentError("CUDA not available.")
+    print("CUDA is available.")
+
+def run_inference(output_path):
+    check_cuda()
+    print("Running inference... (placeholder)")
+    nnUNet_raw = os.path.join('nnUNet_raw')
+    os.environ["nnUNet_results"] = ('.')
+    os.environ["nnUNet_preprocessed"] = os.path.join('.')
+    os.environ["nnUNet_raw"] = os.path.join('.')
+    os.system(f"nnUNetv2_predict -i {nnUNet_raw} -o {output_path} -d 13 -c 3d_fullres -f all -tr nnUNetTrainerDiceCELoss_noSmooth -npp 1 -nps 1 --disable_tta --disable_progress_bar")
